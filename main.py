4b3dffd092919c0b515d2f019547b57079a8e465
tarinikonanki <tarini.konanki@gmail.com>
2025-09-19 03:28:16 +0000

    created basic chatbot template in main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+
+print("Welcome to the Elite 101 Chatbot!")
+
+name = input("Enter your name: ")
+
+age = int(input(f"Welcome, {name}, it's great to meet you!  How old are you? "))
+
+print(f"Well, {name}, it's great to talk to you.  It must be fun being {age}!  How can I help you today?")
+
+print("Please choose from the following options: \n 1. Option 1 \n 2. Option 2 \n 3. Option 3 \n 4. Exit conversation")
+
+optionChosen = int(input("Enter the number of which option you choose: "))
+
+if optionChosen == 4:
+    print("Well, goodbye!  Ending conversation now.")
+    sys.exit()
