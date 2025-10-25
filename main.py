031cd4d7e2fda3536ee54fd431005d116e96bd37
tarinikonanki <tarini.konanki@gmail.com>
2025-10-25 02:45:34 +0000

    Changed focus of chatbot to be focused on helping users navigate an application and added new functionalities for options that user chooses

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
 import sys
 
-print("Welcome to Frisco! Are you ready to learn information about schools here?")
-
-registered_users = {{y, x}}
-
-def login():
-    username = input("Enter your username: ")
-    password = input("Enter your password: ")
-    if(username in registered users)
-
-login()
+print("Welcome to the app! Are you ready to learn information about this application?")
 
 name = input("Enter your name: ")
 
-age = int(input(f"Welcome, {name}, it's great to meet you!  How old are you? "))
+print(f"Well, {name}, it's great to meet you! How can I help you today?")
 
-print(f"Well, {name}, it's great to talk to you.  It must be fun being {age}!  How can I help you today?")
-
-print("Please choose from the following options: \n 1. Option 1 \n 2. Option 2 \n 3. Option 3 \n 4. Exit conversation")
+print("Please choose from the following options: \n 1. Check application features \n 2. Ask questions \n 3. Submit feedback \n 4. Exit conversation")
 
 optionChosen = int(input("Enter the number of which option you choose: "))
 
-if optionChosen == 4:
+if optionChosen == 1:
+    print("Here is a list of the features offered by this application: \n 1. ")
+elif optionChosen == 2:
+    print(f"{name}, what problem are you facing today?")
+    problem = int(input("Please choose from the following options: "))
+elif optionChosen == 4:
     print("Well, goodbye!  Ending conversation now.")
     sys.exit()
